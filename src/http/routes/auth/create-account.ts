72debcf337b5de615ea9d263ab13b5004b926c75
Marcos Parreiras <marcosalp1998@gmail.com>
2024-05-01 15:48:39 -0300

    refactor(http): create-account route

--- src/http/routes/auth/create-account.ts
+++ src/http/routes/auth/create-account.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from "fastify";
 import type { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
+import { hash } from "bcryptjs";
+import { primsa } from "@/lib/prisma";
 
 export async function createAccountRoute(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -14,8 +16,28 @@ export async function createAccountRoute(app: FastifyInstance) {
         }),
       },
     },
-    (request, reply) => {
-      return "User created";
+    async (request, reply) => {
+      const { name, email, password } = request.body;
+      const userWithSameEmail = await primsa.user.findUnique({
+        where: { email },
+      });
+      if (userWithSameEmail) {
+        return reply
+          .status(400)
+          .send({ message: `User with email ${email} already exists` });
+      }
+      const passwordHash = await hash(password, 6);
+      const { id: userId } = await primsa.user.create({
+        data: {
+          email,
+          name,
+          passwordHash,
+        },
+        select: {
+          id: true,
+        },
+      });
+      return reply.status(201).send({ userId });
     }
   );
 }
