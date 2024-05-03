2975510cfcf17eb0a345ed9701bb474eee13e681
Marcos Parreiras <marcosalp1998@gmail.com>
2024-05-03 11:44:54 -0300

    feat(http): add get-user-membership on auth middleware

--- src/@types/fastify.d.ts
+++ src/@types/fastify.d.ts
@@ -1,7 +1,11 @@
 import "fastify";
+import { Organization, Member } from "@prisma/client";
 
 declare module "fastify" {
   export interface FastifyRequest {
     getCurrentUserId(): Promise<string>;
+    getUserMembership(
+      organizationId
+    ): Promise<{ organization: Organization; membership: Member }>;
   }
 }
--- src/http/middlewares/auth.ts
+++ src/http/middlewares/auth.ts
@@ -1,11 +1,39 @@
 import type { FastifyRequest } from "fastify";
 import { UnauthorizedError } from "../routes/_erros/unauthorized_error";
+import { prisma } from "@/lib/prisma";
+import { BadRequestError } from "../routes/_erros/bad_request_error";
 
 export async function authMiddleware(request: FastifyRequest) {
   request.getCurrentUserId = async () => {
     try {
       const { sub } = await request.jwtVerify<{ sub: string }>();
       return sub;
+    } catch (error: unknown) {
+      throw new UnauthorizedError("Invalid auth token");
+    }
+  };
+
+  request.getUserMembership = async (slug: string) => {
+    try {
+      const { sub } = await request.jwtVerify<{ sub: string }>();
+      const member = await prisma.member.findFirst({
+        where: {
+          userId: sub,
+          organization: {
+            slug,
+          },
+        },
+        include: {
+          organization: true,
+        },
+      });
+      if (!member) {
+        throw new UnauthorizedError(
+          "You are not a member of this organization"
+        );
+      }
+      const { organization, ...membership } = member;
+      return { organization, membership };
     } catch (error: unknown) {
       throw new UnauthorizedError("Invalid auth token");
     }
